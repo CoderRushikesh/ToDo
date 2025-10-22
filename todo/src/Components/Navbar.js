278df922ea9d19e754993720ef0d8ed045922f3c
CoderRushikesh <rushikamble462@gmail.com>
2025-10-23 03:47:07 +0530

    Added navbar Component

---
+++ todo/src/Components/Navbar.js
@@ -0,0 +1,11 @@
+// src/components/Navbar.js
+import React from 'react';
+import './Navbar.css';
+
+const Navbar = () => (
+  <nav className="navbar">
+    <h2>📝 My ToDo App</h2>
+  </nav>
+);
+
+export default Navbar;

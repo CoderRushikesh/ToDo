23e5bdc9c5e00da2598c28d0c9e3d7a1f4f5292f
CoderRushikesh <rushikamble462@gmail.com>
2025-10-23 04:02:49 +0530

    NoteCard component added

---
+++ todo/src/Components/NoteCard.js
@@ -0,0 +1,12 @@
+
+import React from "react";
+import './NoteCard.css';
+
+const NoteCard = () => (
+     <div className="note-card">
+
+   <p> here enter the text </p>
+<button> To add text or Note </button>
+
+     </div>
+)
